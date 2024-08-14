9bc905c8eea2b82b530154a12c0b39f1bece0c6a
William Wun <williamwun@gmail.com>
2024-08-13 22:50:31 -0700

    project started

---
+++ src/main/java/com/william/booking/bill/springboot/booking_springboot/BookingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.william.booking.bill.springboot.booking_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/booking/bill/springboot/booking_springboot/BookingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.booking.bill.springboot.booking_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
